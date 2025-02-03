e9a88ad74f1457a356091306c620b3ff305d0530
Martin <changeme@example.com>
2025-02-03 14:45:57 +0000

    Add test file

---
+++ test_pycalc.py
@@ -0,0 +1,14 @@
+import unittest
+
+from pycalc import *
+
+class pycalc_test(unittest.TestCase):
+    '''test module to test calculation functions in `pycalc.py`'''
+
+    def test_addition(self):
+
+    def test_subtraction(self):
+
+    def test_division(self):
+
+    def test_multiplication(self):
