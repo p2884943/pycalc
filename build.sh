4399758fd5b23de14ca6b8361c558441b98a81a4
Martin <changeme@example.com>
2025-02-03 14:45:34 +0000

    Add Docker build script

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mkdir tempdir
+mkdir tempdir/templates
+mkdir tempdir/static
+
+cp pycalc.py tempdir/.
+cp -r templates/* tempdir/templates/.
+cp -r static/* tempdir/static/.
+echo "FROM python" >> tempdir/Dockerfile
+echo "RUN pip install flask" >> tempdir/Dockerfile
+echo "COPY ./static /home/pycalc/static/" >> tempdir/Dockerfile
+echo "COPY ./templates /home/pycalc/templates/" >> tempdir/Dockerfile
+echo "COPY sample_app.py /home/pycalc/" >> tempdir/Dockerfile
+echo "EXPOSE 5050" >> tempdir/Dockerfile
+echo "CMD python3 /home/pycalc/pycalc.py" >> tempdir/Dockerfile
+
+cd tempdir
+
+docker build -t pycalc .
+
+docker run -t -d -p 5050:5050 --name pycalcrunning pycalc
+docker ps -a
