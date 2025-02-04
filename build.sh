f432a73807e65269bd5b7d81e4036c73338b7950
Martin <changeme@example.com>
2025-02-04 10:36:28 +0000

    Remove unnecessary folders

--- build.sh
+++ build.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
 mkdir tempdir
-mkdir tempdir/templates
-mkdir tempdir/static
 
 cp pycalc.py tempdir/.
-cp -r templates/* tempdir/templates/.
-cp -r static/* tempdir/static/.
 echo "FROM python" >> tempdir/Dockerfile
 echo "RUN pip install flask" >> tempdir/Dockerfile
-echo "COPY ./static /home/pycalc/static/" >> tempdir/Dockerfile
-echo "COPY ./templates /home/pycalc/templates/" >> tempdir/Dockerfile
 echo "COPY sample_app.py /home/pycalc/" >> tempdir/Dockerfile
 echo "EXPOSE 5050" >> tempdir/Dockerfile
 echo "CMD python3 /home/pycalc/pycalc.py" >> tempdir/Dockerfile
