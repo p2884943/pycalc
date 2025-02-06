d5c65fb6097eda232431d3a5ea4eacbb4a926610
Martin <changeme@example.com>
2025-02-06 14:04:56 +0000

    Uncomment tests

--- test_pycalc.py
+++ test_pycalc.py
@@ -13,13 +13,13 @@ class pycalc_test(unittest.TestCase):
         '''the returned difference should be the equal to the test case'''
         self.assertTrue(11==subtraction(15, 4))
 
-   # def test_division(self):
-   #     '''the returned quotient should be equal to the test case'''
-   #     self.assertTrue(11==division(55, 5))
+    def test_division(self):
+        '''the returned quotient should be equal to the test case'''
+        self.assertTrue(11==division(55, 5))
 
-   # def test_multiplication(self):
-   #     '''the returned product should be equal to the test case'''
-   #     self.assertTrue(11==multiplication(5.5, 2))
+    def test_multiplication(self):
+        '''the returned product should be equal to the test case'''
+        self.assertTrue(11==multiplication(5.5, 2))
 
 if __name__ == '__main__':
     unittest.main()
