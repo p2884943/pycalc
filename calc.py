591f9969e9c6a72342ef039a6de06bf14e4d5f88
Martin <changeme@example.com>
2025-02-06 13:42:58 +0000

    Move functions to top of file

--- calc.py
+++ calc.py
@@ -3,3 +3,9 @@ def addition(num1, num2):
 
 def subtraction(num1, num2):
     return num1 - num2
+
+def division(num1, num2):
+    return num1 / num2
+
+def multiplication(num1, num2):
+    return num1 * num2
